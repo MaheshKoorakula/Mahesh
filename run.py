e8889ca324f5bf7f94815933e3ea1023a598d813
MaheshKoorakula <maheshkoorakula@gmail.com>
2021-02-03 16:18:27 +0530

    Fix heroku errors

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import app
 from db import db
 
-db.ini(app)
+db.init_app(app)
 
 @app.before_first_request
 def create_tables(): # This will execute before the first request can be made and will create all the tables which are required in the script.
